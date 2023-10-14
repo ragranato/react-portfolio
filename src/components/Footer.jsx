fa33577a3c050addba76f7dd3e0ff9894233fa37
ragranato <robert.granato@gmail.com>
2023-10-14 16:33:58 -0400

    completed links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,32 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import '../styles/Footer.scss'
+import Pdf from '../assets/Resume_Roberto_Granato.pdf' 
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <EmailIcon />
-        <LinkedInIcon />
-        <GitHubIcon />
-        <PictureAsPdfIcon />
+        <a href="mailto:robert.granato@gmail.com" title="Send an Email">
+          <EmailIcon />
+        </a>
+        <a
+          href="http://www.linkedin.com/in/roberto-granato"
+          target="_blank"
+          title="LinkedIn"
+        >
+          <LinkedInIcon />
+        </a>
+        <a href="http://github.com/ragranato" target="_blank" title="GitHub">
+          <GitHubIcon />
+        </a>
+        <a
+          href={Pdf}
+          target="_blank"
+          title="Resume"
+        >
+          <PictureAsPdfIcon />
+        </a>
       </div>
       <p> &copy;2023 Roberto Granato </p>
     </div>
